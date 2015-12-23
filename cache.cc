98679609bda8498d6dca75a4f7941996b22ab4f0
zch <467883442@qq.com>
2015-12-23 13:25:09 +0800

    add cache

---
+++ cache.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<map>
+#include<list>
+using namespace std;
+
+class solution
+{
+struct CacheNode
+{
+int 
+};
+private:
+list<TreeNode> tl; 
+map<key,list<TreeNode>::iterator> cache
+};
